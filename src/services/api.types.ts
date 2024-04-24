ced56c4dc3a72f15d1a848d539b35cc31fa43e40
sno0wi <sno0wi20@gmail.com>
2024-04-24 17:24:02 +0300

    api done

---
+++ src/services/api.types.ts
@@ -0,0 +1,11 @@
+export interface RequestImgParamsProps {
+  searchTerm: string;
+  page: number;
+}
+
+export interface ParamsTypes {
+    client_id: string;
+    query: string;
+    per_page: number;
+    page: number;
+}
